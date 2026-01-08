979f97c16c799600589af8abc06d6b27420279e5
ahmettalhadinc <matbaahmet@gmail.com>
2026-01-08 18:43:49 +0300

    text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import Navbar from "./components/Navbar";
 import Options from "./components/Options";
 
-
 export default function Home() {
+  const today = new Date().toLocaleDateString("tr-TR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
-   <>
-   <div>
-   <Options/>
-   </div>
-   </>
+    <>
+      <div>
+        <Navbar />
+        
+
+        <Options />
+        <p style={{ margin: "10px 0", fontSize: "14px", color: "#666", textAlign:'center' }}>
+          {today} Reçelli Dükkan
+        </p>
+      </div>
+    </>
   );
 }
