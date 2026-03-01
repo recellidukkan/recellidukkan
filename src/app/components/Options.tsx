09d47ddbf2158413082a6a6fb785a83291a1619c
ahmettalhadinc <matbaahmet@gmail.com>
2026-03-01 22:44:24 +0300

    sandivc removed

--- src/app/components/Options.tsx
+++ src/app/components/Options.tsx
@@ -16,7 +16,7 @@ const Options = () => {
         { name: 'Soğuk İçecekler', photo: '/soguk1.jpg', url: '/soguk' },
         { name: 'Reçelli Dükkan Special', photo: '/special.jpg', url: '/tatli' },
         { name: 'Sandviçler', photo: '/sandvic1.jpg', url: '/sandvic' },
-        { name: 'Reçeller', photo: '/recel.jpg', url: '/recel' },
+        // { name: 'Reçeller', photo: '/recel.jpg', url: '/recel' },
         // { name: 'Kahvaltılar', photo: '/kahvalti.jpg', url: '/kahvalti' },
     ]
 
